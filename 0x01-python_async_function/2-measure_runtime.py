ff78f26a0405a824d507e663ae9aeeead8cbf62c
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-10-15 15:21:47 +0000

    measure runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
+''' Measures the total execution time for wait_n(n, max_delay)
+and returns the average time per call.
+
+    Parameters:
+    n (int): The number of times to spawn wait_random.
+    max_delay (int): The maximum delay in seconds.
+
+    Returns:
+    float: The average time per call.
+'''
 import asyncio
-import random
 import time
-from typing import List
 
 
-async def wait_random(max_delay: int) -> float:
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
-
-
-async def wait_n(n: int, max_delay: int) -> List[float]:
-    delays = []
-    for _ in range(n):
-        delay = await wait_random(max_delay)
-        for i, d in enumerate(delays):
-            if delay < d:
-                delays.insert(i, delay)
-                break
-        else:
-            delays.append(delay)
-    return delays
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
+    '''Computes the average runtime of wait_n.
+    '''
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total_time = end_time - start_time
-    return total_time / n
+    return (time.time() - start_time) / n
