25272a816258c8ba3be8c3ec246a5732b2676e4c
MbongeniChristian3 <mbongenichristian3@gmail.com>
2024-10-15 09:55:19 +0000

    async comprehensions

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import random
+import asyncio
+
+
+async def async_generator():
+    """Asynchronously yield a random float between 0 and 10, ten times."""
+    for _ in range(10):
+        await asyncio.sleep(1)  # Asynchronously wait for 1 second
+        yield random.uniform(0, 10)
